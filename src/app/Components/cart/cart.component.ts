15e48ad3fb8fa4a307d0124ad33910830b9d8cc0
Ibram Reda <abramreda815@gmail.com>
2022-11-09 13:31:52 +0200

    using ngModelChange to track changes

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -1,41 +1,41 @@
 import { UserInfo } from './../../Models/user-info';
 import { CartItem } from './../../Models/cart-item';
-import { Product } from './../../Models/product';
 import { ShoppingCartService } from './../../Services/shopping-cart.service';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  cartItems:CartItem[] = [];
-  confirmed:boolean = false;
-  userInfo:UserInfo={
-    fullName:'',
-    address:'',
-    creditCardNumber:0
+  cartItems: CartItem[] = [];
+  confirmed: boolean = false;
+  userInfo: UserInfo = {
+    fullName: '',
+    address: '',
+    creditCardNumber: 0,
   };
   FinalTotalValue = 0;
-  constructor(private shoppingCart:ShoppingCartService) { }
+  ControlesWithErrors: string[] = [];
+
+  constructor(private shoppingCart: ShoppingCartService) {}
 
   ngOnInit(): void {
     this.cartItems = this.shoppingCart.CartItems;
   }
 
-  get Total():number
-  {
+  get Total(): number {
     let total = 0;
-    this.cartItems.forEach(item=>{
+    this.cartItems.forEach((item) => {
       total += item.Count * item.product.price;
-    })
+    });
     return total;
   }
 
-  OnSubmit(formData:NgForm){
-    if(formData.invalid){
+  OnSubmit(formData: NgForm) {
+    if (formData.invalid) {
       formData.form.markAllAsTouched();
       return;
     }
@@ -44,7 +44,18 @@ export class CartComponent implements OnInit {
     this.shoppingCart.Clear();
   }
 
-  OnDelete(ItemId:number){
+  OnDelete(ItemId: number) {
     this.shoppingCart.remove(ItemId);
+  }
+
+  OnModelChange(controlData: NgModel) {
+    const controlName = controlData.name;
+    const ErrorIndx = this.ControlesWithErrors.findIndex(a => a === controlName);
+    if (controlData.invalid && ErrorIndx < 0) {
+      this.ControlesWithErrors.push(controlName);
+      return;
+    }
+    if (controlData.valid && ErrorIndx >= 0)
+      this.ControlesWithErrors.splice(ErrorIndx, 1);
   }
 }
